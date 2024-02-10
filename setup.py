d5488a256eccf51018831f0ca5c16b537f3cd181
Kamal Jnawali <kamaljnawali@Kamals-MacBook-Pro.local>
2024-02-10 10:59:02 -0500

    add app

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 
 __version__ = "0.0.0"
+
 REPO_NAME = "cnnClassifier"
 AUTHOR_USER_NAME = "kj14300"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = ""
+
 
 setuptools.setup(
     name=SRC_REPO,
@@ -19,11 +21,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-
-    project_urls=
-    {
-        "Bug Trakcer": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
--- template.py
+++ template.py
@@ -1,14 +1,13 @@
-import os 
-from pathlib import Path 
-import logging 
+import os
+from pathlib import Path
+import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 project_name = "cnnClassifier"
 
-
 list_of_files = [
-    ".github/workflow/.gitkeep",
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/utils/__init__.py",
@@ -24,18 +23,24 @@ list_of_files = [
 
 ]
 
+
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir  != "":
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
     
-    if not os.path.exists(filepath) or os.path.getsize(filepath)== 0:
-        with open(filepath, "w") as f:
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass #creating an empty file only
             logging.info(f"Creating empty file: {filepath}")
-            pass
+    
     else:
-        logging.info(f"{filename} is already exist")
+        logging.info(f"{filename} is already exists")
+
+
+
 
